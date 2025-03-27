7b5038dbe130bdd695deed6e29803c88ef28bea6
GabrielEmidi023 <gabriel.e.oliveira@iesb.edu.br>
2025-03-26 21:46:24 -0300

    qualquer coisa

---
+++ fundamento/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PrimeiroComponente() {
+  return (
+    <View>
+      <Text>PrimeiroComponente</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+container: {
+    backgroundColor: 'red'
+}
+texto: {
+    
+}
