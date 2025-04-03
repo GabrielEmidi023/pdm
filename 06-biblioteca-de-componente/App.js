5a10e899e39ae105acdbb83bc3971b3f77101088
GabrielEmidi023 <gabriel.e.oliveira@iesb.edu.br>
2025-04-02 21:25:11 -0300

    mandando aula chata

---
+++ 06-biblioteca-de-componente/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Button, Card, Divider, PaperProvider, Paragraph, Text, Title } from 'react-native-paper';
+
+export default function App() {
+  
+  const frutas = ["uva", "MAÇA", "Laranja", "MEÇÃO"]
+
+  return (
+    <PaperProvider>
+
+      
+
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
